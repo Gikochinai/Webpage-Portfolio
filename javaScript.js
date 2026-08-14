0454af87fc745220dcb187763a039889e7d717fc
Gikochinai <98325710+Gikochinai@users.noreply.github.com>
2026-08-14 23:52:28 +0800

    GameDev Section layout testing

--- javaScript.js
+++ javaScript.js
@@ -1,19 +1,43 @@
+// Page fade-in
 window.addEventListener('load', () => {
-    document.querySelector('.body1').classList.add('page-loaded');
+
+    const body1 = document.querySelector('.body1');
+
+    if (body1) {
+        body1.classList.add('page-loaded');
+    }
+
 });
 
+
+// Navigation link transition
 document.querySelectorAll('a').forEach(link => {
+
     link.addEventListener('click', function (event) {
+
         event.preventDefault();
 
         const destination = this.href;
         const body1 = document.querySelector('.body1');
 
-        body1.classList.remove('page-loaded');
-        body1.classList.add('fade-out');
+        if (body1) {
+            body1.classList.remove('page-loaded');
+            body1.classList.add('fade-out');
+        }
 
         setTimeout(() => {
             window.location.href = destination;
         }, 500);
+
     });
+
+});
+
+
+$(document).ready(function () {
+
+    $(".box1").click(function () {
+        window.location.href = "GameDev.html";
+    });
+
 });
